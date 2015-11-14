3c5255c146b07955fa92095380e0e51ffbf69059
Emily Shepherd <emily@emilyshepherd.me>
2015-11-14 18:56:19 +0000

    Removed unused imports

--- src/weka/core/converters/ArffLoader.java
+++ src/weka/core/converters/ArffLoader.java
@@ -29,7 +29,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
 import java.io.StreamTokenizer;
-import java.text.ParseException;
 
 import weka.core.Attribute;
 import weka.core.FastVector;
